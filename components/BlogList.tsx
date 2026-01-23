84abaf2e9f17790cb5f41f0447c2da2cd0beb9ac
Lucian Bellevue <romeomdna@outlook.com>
2026-01-23 11:53:16 -0500

    bloglist column update

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -83,7 +83,7 @@ export default function BlogList({ posts, categories }: BlogListProps) {
 
       {/* Posts Grid */}
       {filteredPosts.length > 0 ? (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
           {filteredPosts.map((post) => (
             <PostCard key={post.slug} post={post} />
           ))}
